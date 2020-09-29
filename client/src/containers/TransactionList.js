4dfe1c0ba5f2dd477129988ce39cec92b872c435
Xavier Lu <xll2@cornell.edu>
2020-09-29 19:45:44 -0400

    modified transcation list

--- client/src/containers/TransactionList.js
+++ client/src/containers/TransactionList.js
@@ -14,19 +14,15 @@ const useStyles = makeStyles({
   },
 });
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
 const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  {
+    address: "0x29a34b823c87d289e758f9f24f75a8a927e8b9",
+    amount: "100",
+    timestamp: "12:00:00",
+  },
 ];
 
-export default function TransactionList() {
+const TransactionList = (props) => {
   const classes = useStyles();
 
   return (
@@ -34,27 +30,25 @@ export default function TransactionList() {
       <Table className={classes.table} size="small" aria-label="a dense table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>Receipent</TableCell>
+            <TableCell align="right">Amount</TableCell>
+            <TableCell align="right">Timestamp</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
-            <TableRow key={row.name}>
+            <TableRow key={row.address}>
               <TableCell component="th" scope="row">
-                {row.name}
+                {row.address}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">{row.amount}</TableCell>
+              <TableCell align="right">{row.timestamp}</TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
     </TableContainer>
   );
-}
+};
+
+export default TransactionList;
