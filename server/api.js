120b6841662eb164681f429fc38fe52ba296efae
Xavier Lu <xll2@cornell.edu>
2020-10-23 17:31:53 -0400

    added get balance api endpoint

--- server/api.js
+++ server/api.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const mysql = require('mysql');
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const Wallet = require("./wallet");
+const Block = require("./block");
+const Blockchain = require("./blockchain");
+
 
 //get the port from the user or set the default port
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
@@ -11,6 +15,15 @@ const app = express();
 
 //using the blody parser middleware
 app.use(bodyParser.json());
+
+// db connection
+let conn = mysql.createConnection({
+  host: 'cbdc.cjymkpun4qnd.us-east-1.rds.amazonaws.com',
+  port: 3306,
+  user: 'admin',
+  password: 'cornellblockchain',
+  database: 'blockchain'
+});
 
 //EXPOSED APIs
 
@@ -24,6 +37,34 @@ app.get("/generateWallet", (req, res) => {
 app.get("/getWallet", (req, res) => {
   const w = new Wallet(req.query.privateKey);
   res.json(w.getKeyPairJSON());
+});
+
+app.get("/getBalance", (req, res) => {
+  const w = new Wallet(req.query.privateKey);
+  conn.connect((err) => {
+    if (err) {
+      res.json(err);
+      return;
+    }
+  });
+  conn.query('SELECT * FROM chain', (err, rows) => {
+    if (err) throw err;
+    let chain = [];
+    let data = rows.sort((a, b) => {
+      return a.timestamp - b.timestamp;
+    });
+    if (data) {
+      data.forEach((row) => {
+        let block = new Block(row.timestamp, row.lasthash, JSON.parse(row.data), row.hash);
+        chain.push(block);
+      })
+    } else {
+      chain = [Block.genesis()];
+    }
+    let blockchain = new Blockchain(chain);
+    res.json(w.calculateBalance(blockchain));
+  });
+  
 });
 
 app.post("/mine", (req, res) => {
