11e4a63228fcec076e481ab082387f89e05ce174
Wendy Huang <65386301+WendyH1108@users.noreply.github.com>
2020-10-28 16:26:19 +0800

    Update blockchain.js

--- server/blockchain.js
+++ server/blockchain.js
@@ -1,31 +1,10 @@
 const Block = require("./block.js");
-const con = mysql.createConnection({
-  host: 'cbdc.cjymkpun4qnd.us-east-1.rds.amazonaws.com',
-  user: 'admin',
-  port: 3306,
-  password: 'cornellblockchain',
-  database: 'blockchain'
-});
+const mysql = require('mysql');
 
 class Blockchain {
-  constructor() {
-    this.chain=[];
-    con.connect((err) => {
-       if(err){ console.log('Cannot connect to database');}
-   });
-    con.query('SELECT * FROM chain', (err,rows) => {
-      if(err) throw err;
-      rows.forEach( (row) => {
-        if(row){
-          let block= new Block(row.timestamp, row.lastHash, row.data, row.hash);
-          this.chain.push(block);
-        }
-        else{this.chain = [Block.genesis()];}
-
-        });
-      });
-    }
-
+  constructor(chain) {
+    this.chain = chain;
+  }
 
   addBlock(data) {
     const block = Block.mineBlock(this.chain[this.chain.length - 1], data);
@@ -34,6 +13,11 @@ class Blockchain {
     return block;
   }
 
+  /**
+   * Given a chain, return true if it is valid
+   * @param {array} chain
+   * @returns {boolean}
+   */
   isValidChain(chain) {
     if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis()))
       return false;
@@ -44,7 +28,7 @@ class Blockchain {
       if (
         block.lastHash !== lastBlock.hash ||
         block.hash !== Block.blockHash(block)
-      )
+      ) 
         return false;
     }
 
